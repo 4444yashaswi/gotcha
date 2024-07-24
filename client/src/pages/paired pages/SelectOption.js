f7bd2a954b28e6b3deffd5a22000b592611a6df2
4444yashaswi <4444yashaswi@gmail.com>
2024-07-24 20:45:55 +0530

    UPDATE: Socket Integration, User Listing | FULLY WORKING MODULE

--- client/src/pages/paired pages/SelectOption.js
+++ client/src/pages/paired pages/SelectOption.js
@@ -1,13 +1,78 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Options from "../Options";
 import SelectSubmit from "../SelectSubmit";
+import { useNavigate, useParams } from "react-router-dom";
+import CONSTANTS from "../../Constants/Constants";
+import axios from "../../Axios/Axios";
 
-const SelectOption = () => {
+const SelectOption = ({
+  hasSelectedPlayer,
+  setHasSelectedPlayer,
+  haveAllSelected,
+  setHaveAllSelected,
+}) => {
+  const { SELECT_STATUS } = CONSTANTS;
+  const { roomId, name } = useParams();
+
+  const navigate = useNavigate();
+
   const [hasPlayerSelectedOption, setHasPlayerSelectedOption] = useState(false);
+  const [playerList, setPlayerList] = useState([]);
+
+  // update the player status if they submit an answer
+  useEffect(() => {
+    if (hasSelectedPlayer) {
+      setPlayerList((players) =>
+        players.map((player) =>
+          player?.name === hasSelectedPlayer?.name
+            ? { ...hasSelectedPlayer }
+            : { ...player }
+        )
+      );
+    }
+  }, [hasSelectedPlayer]);
+
+  // change the screen if the isAll is true
+  useEffect(() => {
+    const readyTimeOut = setTimeout(() => {
+      if (haveAllSelected) navigate(`/scores/${roomId}/${name}`);
+    }, 3000);
+
+    return () => {
+      clearTimeout(readyTimeOut);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [haveAllSelected]);
+
+  // get the list of players present in the room
+  const getPlayerList = async () => {
+    const players = await axios.get(
+      `/roomManagement/userList?roomId=${roomId}&userName=${name}&flag=${SELECT_STATUS}`
+    );
+    console.log(players);
+    setPlayerList(
+      players?.data?.userList?.map((player) => ({
+        name: player?.name,
+        avatarColor: player?.avatarColour,
+        ready: player?.status,
+      }))
+    );
+  };
+
+  useEffect(() => {
+    getPlayerList();
+
+    return () => {
+      setHasSelectedPlayer(false);
+      setHaveAllSelected(false);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <>
       {hasPlayerSelectedOption ? (
-        <SelectSubmit nextScreen="/scores" />
+        <SelectSubmit nextScreen="/scores" playerList={playerList} />
       ) : (
         <Options setSubmitted={setHasPlayerSelectedOption} />
       )}
